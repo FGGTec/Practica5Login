370c7be1039748a44fab840930aecdae11522141
Felipe Garcia Gonzalez <a21490838@itmexicali.edu.mx>
2025-04-01 19:15:42 -0700

    parte 2

--- src/app.py
+++ src/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from config import config
+from flask_mysqldb import MySQL
+from models.ModelUsers import ModelUsers
+from models.entities.users import User
+
 app = Flask(__name__)
+db = MySQL(app)
+
 @app.route("/")
 def index():
-    #return "<h1>Login<h1>"
-    #return render_template("base.html")
     return redirect("login")
+"""  
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -14,14 +19,41 @@ def login():
         print(_user)
         print(_pass)
         if _user == "admin" and _pass == "123":
+            flash("Acceso otorgado...", "success")
             return redirect(url_for("home"))
         else:
+            flash("Acceso rechazado...", "danger")
             return render_template("auth/login.html")
     else:
         return render_template("auth/login.html")
+"""
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = User(0, request.form['username'], request.form['password'],0)
+        logged_user = ModelUsers.login(db, user)
+        if logged_user != None:
+            if logged_user.usertype == 1:
+                flash("Acceso a administrador otorgado...", "warning")
+                return redirect(url_for("admin", username=logged_user.username))
+            else:
+                flash("Acceso a usuario otorgado...", "success")
+                return redirect(url_for("home", username=logged_user.username))
+        else:
+            flash("Acceso a usuario rechazado...", "danger")
+            return render_template("auth/login.html")
+    else:
+        return render_template("auth/login.html")
+  
 @app.route("/home")
 def home():
-    return (render_template("home.html"))
+    username = request.args.get("username", "Invitado")
+    return (render_template("home.html", username=username))
+
+@app.route("/admin")
+def admin():
+    username = request.args.get("username", "Invitado")
+    return (render_template("admin.html", username=username))
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
--- src/config.py
+++ src/config.py
@@ -1,3 +1,8 @@
 class DevelopmentConfig():
     DEBUG = True
+    SECRET_KEY = "qhrf$edjYTJ)*21nsThdK"
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "Root-MySQL_7903"
+    MYSQL_DB = "store"
 config = {"development": DevelopmentConfig}
